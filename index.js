9f5be359a248fb39c393c3091e3c2138041f015f
Tiddo Langerak <tiddolangerak@gmail.com>
2023-09-17 10:11:48 +0100

    chore: simplify

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const {
   beforeAll: jBeforeAll,
   beforeEach: jBeforeEach,
-  test: jTest,
-  it: jIt
+  afterAll: jAfterAll,
+  afterEach: jAfterEach,
 } = globalThis;
 
 /**
@@ -17,35 +17,29 @@ function teardown(cb) {
   cleaners.push(cb);
 }
 
-function patchedHook(jBefore, after) {
-  return (fn, ...hookArgs) => {
-    const myCleaners = [];
-
-    jBefore(async (...innerArgs) => {
-      cleaners = myCleaners;
-      await fn(...innerArgs);
-      // Note that we deliberately don't reset `cleaners`.
-      // This is such that tests will have access to the cleaners set up by `beforeEach`
-    }, ...hookArgs);
-
-    after(async() => {
-      for (const cleaner of myCleaners.splice(0, Number.POSITIVE_INFINITY).reverse()) {
-        await cleaner();
-      }
-    });
+function patchHook(jBefore, jAfter) {
+  return (...args) => {
+    setupCleaners(jBefore, jAfter);
+    jBefore(...args);
   }
 }
 
-globalThis.beforeAll = patchedHook(jBeforeAll, afterAll);
+function setupCleaners(jBefore, jAfter) {
+  const myCleaners = [];
 
-/**
- * This `beforeEach` call is to make sure that teardown works for calls made from within tests:
- * calls made within a test needs to run in the `beforeAfter`, just like calls made within `beforeEach`.
- * By calling the patched `beforeEach` at the top-level here, we ensure that we setup a `cleaners` object for each test.
- *
- * (The alternative is to patch each test method as well, which we'd like to avoid doing)
- */
-let patchedEach = patchedHook(jBeforeEach, afterEach);
-patchedEach(() => {});
+  jBefore(() => {
+    cleaners = myCleaners;
+  });
+
+  jAfter(async() => {
+    for (const cleaner of myCleaners.splice(0, Number.POSITIVE_INFINITY).reverse()) {
+      await cleaner();
+    }
+  });
+}
+
+globalThis.beforeAll = patchHook(jBeforeAll, jAfterAll);
+
+setupCleaners(jBeforeEach, jAfterEach);
 
 module.exports = { teardown };
