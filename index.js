59cd1cbd10a8126bb008d418b3f7fa94f57f51fe
Tiddo Langerak <tiddolangerak@gmail.com>
2023-09-17 10:14:53 +0100

    chore: cleaners -> teardowns

--- index.js
+++ index.js
@@ -9,38 +9,38 @@ const {
  * This will contain a collection of callbacks that will need to run at the end of the current "scope"
  * The array will be initialized in the beforeEach/beforeAll blocks
  */
-let cleaners;
+let teardowns;
 function teardown(cb) {
-  if (!cleaners) {
+  if (!teardowns) {
     throw new Error("teardown can only be called from within `beforeAll`, `beforeEach`, `test` or `it`. It cannot be called from concurrent tests.");
   }
-  cleaners.push(cb);
+  teardowns.push(cb);
 }
 
 function patchHook(jBefore, jAfter) {
   return (...args) => {
-    setupCleaners(jBefore, jAfter);
+    setupTeardowns(jBefore, jAfter);
     jBefore(...args);
   }
 }
 
-function setupCleaners(jBefore, jAfter) {
-  const myCleaners = [];
+function setupTeardowns(jBefore, jAfter) {
+  const myTeardowns = [];
 
   jBefore(() => {
-    cleaners = myCleaners;
+    teardowns = myTeardowns;
   });
 
   jAfter(async() => {
-    for (const cleaner of myCleaners.splice(0, Number.POSITIVE_INFINITY).reverse()) {
+    for (const cleaner of myTeardowns.splice(0, Number.POSITIVE_INFINITY).reverse()) {
       await cleaner();
     }
-    cleaners = undefined;
+    teardowns = undefined;
   });
 }
 
 globalThis.beforeAll = patchHook(jBeforeAll, jAfterAll);
 
-setupCleaners(jBeforeEach, jAfterEach);
+setupTeardowns(jBeforeEach, jAfterEach);
 
 module.exports = { teardown };
